87a9c474374a95e1da5cd4fcb1aa780ab1c9f42a
Vivek Pandita <vpandita@eagle.org>
2024-04-22 17:46:01 +0530

    Adding API Initializer code - Spring Boot with Java 17

---
+++ API/fitness-tracker/src/main/java/com/bits/fitnesstracker/fitnesstracker/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bits.fitnesstracker.fitnesstracker;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FitnessTrackerApplication.class);
+	}
+
+}
