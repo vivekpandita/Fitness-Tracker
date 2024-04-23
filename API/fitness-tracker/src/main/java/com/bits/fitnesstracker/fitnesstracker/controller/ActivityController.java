42e9c6ac3c9fb829fef376a863470201b5403636
Vivek Pandita <vpandita@eagle.org>
2024-04-23 19:56:36 +0530

    More changes API Create Update ACTIVITY

--- API/fitness-tracker/src/main/java/com/bits/fitnesstracker/fitnesstracker/controller/ActivityController.java
+++ API/fitness-tracker/src/main/java/com/bits/fitnesstracker/fitnesstracker/controller/ActivityController.java
@@ -1,14 +1,35 @@
 package com.bits.fitnesstracker.fitnesstracker.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bits.fitnesstracker.fitnesstracker.db.ActivityRepository;
+import com.bits.fitnesstracker.fitnesstracker.model.Activity;
 
 @RestController
 @RequestMapping("/activity")
 public class ActivityController {
 	@Autowired
 	private ActivityRepository activityRepository;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Activity getUIMetadata(@PathVariable(name = "id") Long id) {
+		return activityRepository.getActivityById(id);
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public Activity reloadAllBundles(@RequestBody(required = false) Activity body) {
+		return activityRepository.save(body);
+	}
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public List<Activity> list() {
+		return activityRepository.findAll();
+	}
 }
--- API/fitness-tracker/src/main/java/com/bits/fitnesstracker/fitnesstracker/db/ActivityRepository.java
+++ API/fitness-tracker/src/main/java/com/bits/fitnesstracker/fitnesstracker/db/ActivityRepository.java
@@ -1,9 +1,14 @@
 package com.bits.fitnesstracker.fitnesstracker.db;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bits.fitnesstracker.fitnesstracker.model.Activity;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
-	
+//	List<Activity> getAllActivities();
+    Activity getActivityById(Long id);
+//    Activity saveActivity(Activity Activity);
+//    void deleteActivity(Long id);
 }
