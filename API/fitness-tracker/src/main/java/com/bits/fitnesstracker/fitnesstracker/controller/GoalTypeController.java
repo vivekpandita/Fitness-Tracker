f0961c9c788aa6367f6d661705ef0f9afecdb125
Vivek Pandita <vivek_pandita@rediffmail.com>
2024-05-11 19:24:14 +0530

    @CrossOrigin(origins = "*", allowedHeaders = "*")

--- API/fitness-tracker/src/main/java/com/bits/fitnesstracker/fitnesstracker/controller/GoalTypeController.java
+++ API/fitness-tracker/src/main/java/com/bits/fitnesstracker/fitnesstracker/controller/GoalTypeController.java
@@ -3,6 +3,7 @@ package com.bits.fitnesstracker.fitnesstracker.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.bits.fitnesstracker.fitnesstracker.model.GoalType;
 
 @RestController
 @RequestMapping("/goalType")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class GoalTypeController {
 	@Autowired
 	private GoalTypeRepository goalTypeRepository;
